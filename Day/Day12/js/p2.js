d1a6dc738d4cc38f6df235b70590a7397353b00c
shreyag0560-blip <shreyag0560@gmail.com>
2026-08-18 21:38:45 +0530

    named export & default export

---
+++ Day/Day12/js/p2.js
@@ -0,0 +1,7 @@
+import { add,subtract } from "./p1.js"; //Named export
+import multiply from "./p3.js"; //default export
+
+console.log(add(40,60));
+console.log(subtract(90,60));
+
+console.log(multiply(60,60));
